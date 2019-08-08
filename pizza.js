c1b9e6a9dccfff5e9f1dadfae58abd3889c5e782
Terry Kow Zhi Im <terryim@USF-Gold-Wifi-226-59-98.laptops.usf.edu>
2019-08-08 10:57:43 -0400

    more updated js

--- pizza.js
+++ pizza.js
@@ -1,5 +1,5 @@
 function jsonBuilder() {
-    var elements = document.getElementById("size").elements;
+    var elements = document.getElementById("pizzaForm").elements;
     var obj = {};
     for (var i = 0; i < elements.length - 1; i++) {
         var item = elements.item(i);
@@ -11,20 +11,21 @@ function jsonBuilder() {
     var json = JSON.stringify(obj);
     console.log(json);
     return json;
-}
+};
 
 
 
-
-var http = new XMLHttpRequest();
-http.open("POST", 'http://localhost:8080/', true);
-http.send();
-console.log(http);
-
-
-http.onreadystatechange = function monitorState() {
+function sunbmitPress() {
+    var http = new XMLHttpRequest();
+    http.open("POST", 'http://localhost:8080/', true);
+    http.send();
     console.log(http);
-    if (http.readyState == 4 && http.status == 200) {
-        console.log(JSON.parse(http.response));
-    }
+
+
+    http.onreadystatechange = function monitorState() {
+        console.log(http);
+        if (http.readyState == 4 && http.status == 200) {
+            console.log(JSON.parse(http.response));
+        }
+    };
 };
