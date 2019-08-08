d2a3e96c96c9fe42b7732eba99a19087686669a7
Terry Kow Zhi Im <terryim@USF-Gold-Wifi-226-59-98.laptops.usf.edu>
2019-08-08 10:33:57 -0400

    ajax from js

---
+++ pizza.js
@@ -0,0 +1,30 @@
+function jsonBuilder() {
+    var elements = document.getElementById("size").elements;
+    var obj = {};
+    for (var i = 0; i < elements.length - 1; i++) {
+        var item = elements.item(i);
+        obj[item.name] = item.value;
+        console.log(obj);
+
+
+    }
+    var json = JSON.stringify(obj);
+    console.log(json);
+    return json;
+}
+
+
+
+
+var http = new XMLHttpRequest();
+http.open("POST", 'http://localhost:8080/', true);
+http.send();
+console.log(http);
+
+
+http.onreadystatechange = function monitorState() {
+    console.log(http);
+    if (http.readyState == 4 && http.status == 200) {
+        console.log(JSON.parse(http.response));
+    }
+};
